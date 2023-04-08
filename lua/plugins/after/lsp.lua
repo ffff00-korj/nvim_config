e2be59e8f461a691424f6e65ffed0f775570b5bb
Ilya Borovkov <iborovkov97@yandex.ru>
2023-04-08 18:01:26 +0300

    trouble and comments

--- lua/plugins/after/lsp.lua
+++ lua/plugins/after/lsp.lua
@@ -1,8 +1,17 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
-lsp.on_attach(function(client, bufnr)
-  lsp.buffer_autoformat()
+lsp.on_attach(function()
+    lsp.buffer_autoformat()
 end)
 
 lsp.setup()
+
+-- vim.diagnostic.config({
+--    virtual_text = false,
+--    signs = true,
+--    update_in_insert = false,
+--    underline = false,
+--    severity_sort = false,
+--    float = true,
+--})
--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -1,16 +1,16 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use {
+    use 'wbthomason/packer.nvim'
+    use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        requires = {{'nvim-lua/plenary.nvim'}}
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
     use {
         'doums/darcula',
         config = function() vim.cmd('colorscheme darcula') end
     }
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/playground')
     use('theprimeagen/harpoon')
     use('mbbill/undotree')
@@ -19,31 +19,30 @@ return require('packer').startup(function(use)
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v1.x',
         requires = {
-            {'neovim/nvim-lspconfig'},             -- Required
-            {'williamboman/mason.nvim'},           -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-            {'hrsh7th/nvim-cmp'},         -- Required
-            {'hrsh7th/cmp-nvim-lsp'},     -- Required
-            {'hrsh7th/cmp-buffer'},       -- Optional
-            {'hrsh7th/cmp-path'},         -- Optional
-            {'saadparwaiz1/cmp_luasnip'}, -- Optional
-            {'hrsh7th/cmp-nvim-lua'},     -- Optional
+            { 'hrsh7th/nvim-cmp' },                  -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },              -- Required
+            { 'hrsh7th/cmp-buffer' },                -- Optional
+            { 'hrsh7th/cmp-path' },                  -- Optional
+            { 'saadparwaiz1/cmp_luasnip' },          -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },              -- Optional
 
-            {'L3MON4D3/LuaSnip'},             -- Required
-            {'rafamadriz/friendly-snippets'}, -- Optional
+            { 'L3MON4D3/LuaSnip' },                  -- Required
+            { 'rafamadriz/friendly-snippets' },      -- Optional
         }
     }
     use {
         "folke/trouble.nvim",
         requires = "nvim-tree/nvim-web-devicons",
-        config = function()
-        require("trouble").setup {
-        -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        }
-        end
     }
     use "lukas-reineke/indent-blankline.nvim"
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
 end)
