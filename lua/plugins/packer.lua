f20a4a70cfc43cb8227fef2a7c6c024523c023b9
iborovkov <iborovkov97@yandex.ru>
2023-04-15 02:02:23 +0300

    change theme github dark

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -6,10 +6,6 @@ return require('packer').startup(function(use)
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
-    -- use {
-    --     'doums/darcula',
-    --     config = function() vim.cmd('colorscheme darcula') end
-    -- }
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/playground')
     use('theprimeagen/harpoon')
@@ -39,10 +35,20 @@ return require('packer').startup(function(use)
         requires = "nvim-tree/nvim-web-devicons",
     }
     use "lukas-reineke/indent-blankline.nvim"
-    use {
-        'Mofiqul/dracula.nvim',
-        config = function() vim.cmd('colorscheme dracula') end
-    }
+    use({
+        'projekt0n/github-nvim-theme', tag = 'v0.0.7',
+        config = function()
+            vim.cmd('colorscheme github_dark')
+        end
+    })
+     -- use {
+    --     'doums/darcula',
+    --     config = function() vim.cmd('colorscheme darcula') end
+    -- }
+    -- use {
+    --     'Mofiqul/dracula.nvim',
+    --     config = function() vim.cmd('colorscheme dracula') end
+    -- }
     -- use 'Mofiqul/dracula.nvim'
     use {
         'numToStr/Comment.nvim',
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -110,16 +110,16 @@ _G.packer_plugins = {
     path = "/home/ilya/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
-  ["dracula.nvim"] = {
-    config = { "\27LJ\2\n7\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\24colorscheme dracula\bcmd\bvim\0" },
-    loaded = true,
-    path = "/home/ilya/.local/share/nvim/site/pack/packer/start/dracula.nvim",
-    url = "https://github.com/Mofiqul/dracula.nvim"
-  },
   ["friendly-snippets"] = {
     loaded = true,
     path = "/home/ilya/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
+  },
+  ["github-nvim-theme"] = {
+    config = { "\27LJ\2\n;\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\28colorscheme github_dark\bcmd\bvim\0" },
+    loaded = true,
+    path = "/home/ilya/.local/share/nvim/site/pack/packer/start/github-nvim-theme",
+    url = "https://github.com/projekt0n/github-nvim-theme"
   },
   harpoon = {
     loaded = true,
@@ -208,10 +208,10 @@ time([[Defining packer_plugins]], false)
 time([[Config for Comment.nvim]], true)
 try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
 time([[Config for Comment.nvim]], false)
--- Config for: dracula.nvim
-time([[Config for dracula.nvim]], true)
-try_loadstring("\27LJ\2\n7\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\24colorscheme dracula\bcmd\bvim\0", "config", "dracula.nvim")
-time([[Config for dracula.nvim]], false)
+-- Config for: github-nvim-theme
+time([[Config for github-nvim-theme]], true)
+try_loadstring("\27LJ\2\n;\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\28colorscheme github_dark\bcmd\bvim\0", "config", "github-nvim-theme")
+time([[Config for github-nvim-theme]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
