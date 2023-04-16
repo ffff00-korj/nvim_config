1b0fd174e25f633a287933063528c9c959e42cb2
iborovkov <iborovkov97@yandex.ru>
2023-04-16 13:46:29 +0300

    fix colomn color

--- lua/plugins/after/gruvbox.lua
+++ lua/plugins/after/gruvbox.lua
@@ -1,6 +1,9 @@
 require("gruvbox").setup({
     palette_overrides = {
+        -- background
         dark0 = "#1d1f21",
-    }
+        -- colomn
+        dark1 = "#1d1f21",
+    },
 })
 vim.cmd.colorscheme 'gruvbox'
