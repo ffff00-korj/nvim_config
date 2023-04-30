5409285c2833e15e4bd47be77ee12147e06e264f
Ilya Borovkov <iborovkov97@yandex.ru>
2023-04-30 10:54:39 +0300

    add plugin vim notifications

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -171,6 +171,11 @@ _G.packer_plugins = {
     path = "/home/ilya/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
+  ["nvim-notify"] = {
+    loaded = true,
+    path = "/home/ilya/.local/share/nvim/site/pack/packer/start/nvim-notify",
+    url = "https://github.com/rcarriga/nvim-notify"
+  },
   ["nvim-treesitter"] = {
     loaded = true,
     path = "/home/ilya/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
