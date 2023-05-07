489091e1e66f45546a4e0a5b86e01a21063f55fa
Ilya Borovkov <iborovkov97@yandex.ru>
2023-05-08 00:59:49 +0300

    change colors to gruvbox-baby

--- lua/plugins/coloscheme.lua
+++ lua/plugins/coloscheme.lua
@@ -1,27 +1,9 @@
 return {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    lazy = false,
-    priority = 1000,
+    'luisiacc/gruvbox-baby',
+    name = 'gruvbox',
     config = function()
-        vim.opt.laststatus = 3 -- Or 3 for global statusline
-        vim.opt.statusline = " %f %m %= %l:%c "
-        require('rose-pine').setup({
-            variant = 'main',
-            dark_variant = 'main',
-            highlight_groups = {
-                ColorColumn = { bg = 'grey' },
-                CursorLine = { bg = 'foam', blend = 10 },
-                StatusLine = { fg = 'love', bg = 'love', blend = 10 },
-                StatusLineNC = { fg = "subtle", bg = "surface" },
-                TelescopeBorder = { fg = "highlight_high", bg = "none" },
-                TelescopeNormal = { bg = "none" },
-                TelescopePromptNormal = { bg = "base" },
-                TelescopeResultsNormal = { fg = "subtle", bg = "none" },
-                TelescopeSelection = { fg = "text", bg = "base" },
-                TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
-            },
-        })
-        vim.cmd('colorscheme rose-pine')
+        vim.g.gruvbox_baby_function_style = "NONE"
+        vim.g.gruvbox_baby_keyword_style = "italic"
+        vim.cmd.colorscheme('gruvbox-baby')
     end
 }
