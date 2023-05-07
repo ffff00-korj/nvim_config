26fe42111ef900cf9f8b110a3468023a061d1d49
iborovkov <iborovkov97@yandex.ru>
2023-05-08 02:47:57 +0300

    apdate lsp config

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -18,10 +18,29 @@ return {
     },
     keys = {
         { '<leader>f', function() vim.lsp.buf.format { async = true } end },
+        { '<space>e',  vim.diagnostic.open_float },
+        { '[d',        vim.diagnostic.goto_prev },
+        { ']d',        vim.diagnostic.goto_next },
+        { '<space>q',  vim.diagnostic.setloclist },
+        { 'gD',        vim.lsp.buf.declaration },
+        { 'gd',        vim.lsp.buf.definition },
+        { 'K',         vim.lsp.buf.hover },
+        { 'gi',        vim.lsp.buf.implementation },
+        { '<C-k>',     vim.lsp.buf.signature_help },
+        { '<space>wa', vim.lsp.buf.add_workspace_folder },
+        { '<space>wr', vim.lsp.buf.remove_workspace_folder },
+        { '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end },
+        { '<space>D',  vim.lsp.buf.type_definition },
+        { '<space>rn', vim.lsp.buf.rename },
+        { 'gr',        vim.lsp.buf.references },
+
     },
     config = function()
         local lsp = require('lsp-zero')
         lsp.preset('recommended')
+        vim.diagnostic.config({ virtual_text = true, })
 
         lsp.on_attach(function(client, bufnr)
             lsp.default_keymaps({ buffer = bufnr })
@@ -55,7 +74,6 @@ return {
                 },
             },
         }
-
         lspconfig.lua_ls.setup {
             settings = {
                 Lua = {
@@ -66,53 +84,18 @@ return {
             },
         }
 
-        vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-        vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-        vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
-        vim.api.nvim_create_autocmd('LspAttach', {
-            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-            callback = function(ev)
-                vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-                local opts = { buffer = ev.buf }
-                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-                vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-                vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-                vim.keymap.set('n', '<space>wl', function()
-                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                end, opts)
-                vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-                vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-                vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-                vim.keymap.set('n', '<space>f', function()
-                    vim.lsp.buf.format { async = true }
-                end, opts)
-            end,
-        })
-
-
-        vim.diagnostic.config({ virtual_text = true, })
         -- lsp setup
         lsp.setup()
 
+
         local cmp = require('cmp')
-        local cmp_action = require('lsp-zero').cmp_action()
 
         cmp.setup({
             window = {
                 completion = cmp.config.window.bordered(),
             },
             mapping = {
-                ['<Tab>'] = cmp_action.luasnip_supertab(),
-                ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<Enter>'] = cmp.mapping.confirm({ select = true }),
             },
             formatting = {
                 fields = { 'abbr', 'kind', 'menu' },
