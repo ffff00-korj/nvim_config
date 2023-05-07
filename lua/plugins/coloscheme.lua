e63f79f16b0b3ab5a3bcb550a5ac6150d94fdf81
Ilya Borovkov <iborovkov97@yandex.ru>
2023-05-08 00:33:15 +0300

    change colorscheme to rosepine

--- lua/plugins/coloscheme.lua
+++ lua/plugins/coloscheme.lua
@@ -1,17 +1,27 @@
 return {
-    'ellisonleao/gruvbox.nvim',
+    'rose-pine/neovim',
+    name = 'rose-pine',
     lazy = false,
-    name = 'gruvbox',
+    priority = 1000,
     config = function()
-        vim.cmd.colorscheme('gruvbox')
-        require('gruvbox').setup({
-            palette_overrides = {
-                -- background
-                dark0 = '#1d1f21',
-                -- colomn
-                dark1 = '#1d1f21',
+        vim.opt.laststatus = 3 -- Or 3 for global statusline
+        vim.opt.statusline = " %f %m %= %l:%c "
+        require('rose-pine').setup({
+            variant = 'main',
+            dark_variant = 'main',
+            highlight_groups = {
+                ColorColumn = { bg = 'grey' },
+                CursorLine = { bg = 'foam', blend = 10 },
+                StatusLine = { fg = 'love', bg = 'love', blend = 10 },
+                StatusLineNC = { fg = "subtle", bg = "surface" },
+                TelescopeBorder = { fg = "highlight_high", bg = "none" },
+                TelescopeNormal = { bg = "none" },
+                TelescopePromptNormal = { bg = "base" },
+                TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+                TelescopeSelection = { fg = "text", bg = "base" },
+                TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
             },
         })
-        vim.cmd.colorscheme('gruvbox')
+        vim.cmd('colorscheme rose-pine')
     end
 }
--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -23,7 +23,6 @@ return {
             lsp.buffer_autoformat()
         end)
 
-
         vim.diagnostic.config({ virtual_text = true, })
 
         local lspconfig = require('lspconfig')
@@ -48,36 +47,27 @@ return {
                 },
             },
         }
-        vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-        vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-        vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
-        vim.api.nvim_create_autocmd('LspAttach', {
-            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-            callback = function(ev)
-                vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-                local opts = { buffer = ev.buf }
-                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-                vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-                vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-                vim.keymap.set('n', '<space>wl', function()
-                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                end, opts)
-                vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-                vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-                vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-                vim.keymap.set('n', '<space>f', function()
-                    vim.lsp.buf.format { async = true }
-                end, opts)
-            end,
-        })
         lsp.setup()
+
+        local cmp = require('cmp')
+        cmp.setup({
+            window = {
+                completion = {
+                    autocomplete = false,
+                },
+                mapping = {
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                },
+                documentation = cmp.config.window.bordered(),
+            },
+            formatting = {
+                fields = { 'abbr', 'kind', 'menu' },
+                format = require('lspkind').cmp_format({
+                    mode = 'symbol',
+                    maxwidth = 50,
+                    ellipsis_char = '...',
+                })
+            }
+        })
     end
 }
