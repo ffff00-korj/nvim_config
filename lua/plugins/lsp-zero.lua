c3371e83ba0e2936977e3551416b7ccbff8b2743
iborovkov <iborovkov97@yandex.ru>
2023-05-08 02:25:39 +0300

    apdate lsp config

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -3,6 +3,7 @@ return {
     lazy = false,
     dependencies = {
         { 'neovim/nvim-lspconfig' },
+        { 'lukas-reineke/lsp-format.nvim' },
         { 'williamboman/mason.nvim' },
         { 'williamboman/mason-lspconfig.nvim' },
         { 'hrsh7th/nvim-cmp' },
@@ -15,17 +16,33 @@ return {
         { 'rafamadriz/friendly-snippets' },
         { 'onsails/lspkind-nvim' },
     },
+    keys = {
+        { '<leader>f', function() vim.lsp.buf.format { async = true } end },
+    },
     config = function()
         local lsp = require('lsp-zero')
         lsp.preset('recommended')
 
-        lsp.on_attach(function()
-            lsp.buffer_autoformat()
+        lsp.on_attach(function(client, bufnr)
+            lsp.default_keymaps({ buffer = bufnr })
+
+            local allow_format = { 'lua_ls', 'rust_analyzer', 'python' }
+            if vim.tbl_contains(allow_format, client.name) then
+                require('lsp-format').on_attach(client)
+            end
         end)
 
-        vim.diagnostic.config({ virtual_text = true, })
+        lsp.set_sign_icons({
+            error = '',
+            warn = ' ',
+            hint = '',
+            info = ''
+        })
 
+
+        -- lsp config lsp's
         local lspconfig = require('lspconfig')
+
         lspconfig.pylsp.setup {
             settings = {
                 pylsp = {
@@ -38,6 +55,7 @@ return {
                 },
             },
         }
+
         lspconfig.lua_ls.setup {
             settings = {
                 Lua = {
@@ -47,18 +65,54 @@ return {
                 },
             },
         }
+
+        vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+        vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+            callback = function(ev)
+                vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+                local opts = { buffer = ev.buf }
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+                vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                vim.keymap.set('n', '<space>wl', function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                end, opts)
+                vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+                vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+                vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                vim.keymap.set('n', '<space>f', function()
+                    vim.lsp.buf.format { async = true }
+                end, opts)
+            end,
+        })
+
+
+        vim.diagnostic.config({ virtual_text = true, })
+        -- lsp setup
         lsp.setup()
 
         local cmp = require('cmp')
+        local cmp_action = require('lsp-zero').cmp_action()
+
         cmp.setup({
             window = {
-                completion = {
-                    autocomplete = false,
-                },
-                mapping = {
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                },
-                documentation = cmp.config.window.bordered(),
+                completion = cmp.config.window.bordered(),
+            },
+            mapping = {
+                ['<Tab>'] = cmp_action.luasnip_supertab(),
+                ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
             },
             formatting = {
                 fields = { 'abbr', 'kind', 'menu' },
