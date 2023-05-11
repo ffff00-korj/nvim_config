f4e15d03971f2698f8ed8aee0b4ef876f83d45fc
Ilya Borovkov <iborovkov97@yandex.ru>
2023-05-11 20:32:01 +0300

    add split switching

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,8 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
+-- Better split switching
+vim.keymap.set('', '<C-j>', '<C-W>j')
+vim.keymap.set('', '<C-k>', '<C-W>k')
+vim.keymap.set('', '<C-h>', '<C-W>h')
+vim.keymap.set('', '<C-l>', '<C-W>l')
