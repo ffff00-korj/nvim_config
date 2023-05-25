5bba7e89067b8a082f57d6ffba6e73de8f9f1905
iborovkov <iborovkov97@yandex.ru>
2023-05-25 11:40:58 +0300

    configs

---
+++ lua/plugins/swenv.lua
@@ -0,0 +1,14 @@
+return {
+    'AckslD/swenv.nvim',
+    dependencies = {
+        'stevearc/dressing.nvim',
+    },
+    keys = {
+        { '<leader>dC', "<cmd>lua require('swenv.api').pick_venv()<cr>" },
+    },
+    config = function()
+        require('swenv').setup({
+            venvs_path = vim.fn.expand('~/.pyenv/versions/*/envs/')
+        })
+    end
+}
