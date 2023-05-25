bed169a962bddb886d048e1d27b8f14546a4b9b5
iborovkov <iborovkov97@yandex.ru>
2023-05-25 17:00:10 +0300

    add surround keys

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,8 +2,6 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        require('lualine').setup {
-            options = { theme = 'vscode' },
-        }
+        require('lualine').setup { options = { theme = 'vscode' }, }
     end
 }
