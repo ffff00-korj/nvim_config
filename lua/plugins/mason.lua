dc4bb51db474568378e78af6147dbfa43b0c2b1a
iborovkov <iborovkov97@yandex.ru>
2023-05-25 17:04:11 +0300

    add Mason components

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,6 +3,16 @@ return {
     cmd = "Mason",
     opts = {
         ensure_installed = {
+            'pylsp',
+            'json-lsp',
+            'lua-language-server',
+            'yaml-language-server',
+
+            'black',
+            'djlint',
+            'flake8',
+            'pyflakes',
+
             'debugpy',
         },
     },
