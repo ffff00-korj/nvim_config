b3e43f6b077d0e2b06cc00877d940feaf9abd979
iborovkov <iborovkov97@yandex.ru>
2023-06-05 16:53:00 +0300

    remove split keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,15 +5,15 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 -- Better split switching
--- vim.keymap.set('', '<C-j>', '<C-W>j')
--- vim.keymap.set('', '<C-k>', '<C-W>k')
--- vim.keymap.set('', '<C-h>', '<C-W>h')
--- vim.keymap.set('', '<C-l>', '<C-W>l')
+-- <C-W>k
+-- <C-W>j
+-- <C-W>h
+-- <C-W>l
 -- Better split sizing
--- vim.keymap.set('', '<C-S-K>', '<C-W>+')
--- vim.keymap.set('', '<C-S-J>', '<C-W>-')
--- vim.keymap.set('', '<C-S-L>', '<C-W>>')
--- vim.keymap.set('', '<C-S-H>', '<C-W><')
+-- <C-W>+
+-- <C-W>-
+-- <C-W><
+-- <C-W>>
 -- Add split
 -- <C-W>s -- horisontal
 -- <C-W>v -- vertical
