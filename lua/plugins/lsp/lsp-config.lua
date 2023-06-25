a6d7541b54c97e4d720ffcf59f31420a31237905
iborovkov <ilia97borovkov@gmail.com>
2023-06-25 12:52:30 +0300

    fix lsp config

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -1,5 +1,12 @@
 local lspconfig = require('lspconfig')
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig.html.setup {
+    capabilities = capabilities,
+}
+
 lspconfig.pylsp.setup {
     settings = {
         pylsp = {
