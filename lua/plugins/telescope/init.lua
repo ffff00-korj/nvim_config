49e687474ee8943bbe1a5eab2cca0bd13303e61f
iborovkov <ilia97borovkov@gmail.com>
2023-08-13 20:19:37 +0300

    telescope add current buffer search

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -18,9 +18,12 @@ return {
     config = function(_, opts)
         require('telescope').setup(opts)
         require('telescope').load_extension('dap')
+
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        local utils = require("telescope.utils")
+
+        vim.keymap.set('n', '<leader>pf', function() builtin.find_files({ cwd = utils.buffer_dir() }) end, {})
+        vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({
                 search = vim.fn.input('Grep > ')
