9a03fa3295477ad9e1e3e64ed4d4dd7edc8f0786
iborovkov <iborovkov97@yandex.ru>
2023-08-19 17:58:00 +0300

    delete highligt parameters (not inplemented)

--- lua/plugins/dap-ui.lua
+++ lua/plugins/dap-ui.lua
@@ -40,9 +40,9 @@ return {
             dapui.close()
         end
 
-        vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939', bg = '#31353f' })
-        vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef', bg = '#31353f' })
-        vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379', bg = '#31353f' })
+        vim.api.nvim_set_hl(0, 'DapBreakpoint', { fg = '#993939', bg = '#31353f' })
+        vim.api.nvim_set_hl(0, 'DapLogPoint', { fg = '#61afef', bg = '#31353f' })
+        vim.api.nvim_set_hl(0, 'DapStopped', { fg = '#98c379', bg = '#31353f' })
 
         vim.fn.sign_define('DapBreakpoint',
             { text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
