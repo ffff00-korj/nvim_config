36855fc49b5b7306b397e9e0cc0ff78682063480
iborovkov <ilia97borovkov@gmail.com>
2023-08-20 15:27:07 +0300

    delete plugins what i don`t use

--- lua/plugins/git/signs.lua
+++ lua/plugins/git/signs.lua
@@ -10,31 +10,7 @@ local options = {
                     opts.buffer = bufnr
                     vim.keymap.set(mode, l, r, opts)
                 end
-
-                -- Navigation
-                map('n', ']c', function()
-                    if vim.wo.diff then return ']c' end
-                    vim.schedule(function() gs.next_hunk() end)
-                    return '<Ignore>'
-                end, { expr = true })
-
-                map('n', '[c', function()
-                    if vim.wo.diff then return '[c' end
-                    vim.schedule(function() gs.prev_hunk() end)
-                    return '<Ignore>'
-                end, { expr = true })
-
-                -- Actions
-                map('n', '<leader>hr', gs.reset_hunk)
-                map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-                map('n', '<leader>hS', gs.stage_buffer)
-                map('n', '<leader>hR', gs.reset_buffer)
                 map('n', '<leader>hp', gs.preview_hunk_inline)
-                map('n', '<leader>hd', gs.diffthis)
-                map('n', '<leader>hD', function() gs.diffthis('~') end)
-                map('n', '<leader>td', gs.toggle_deleted)
-
-                map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
             end
         }
     end
