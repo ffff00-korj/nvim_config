ddd548d30d01b44cfa25eaad3cd78425e79b41b6
iborovkov <ilia97borovkov@gmail.com>
2023-08-28 22:30:20 +0300

    change quotes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,18 +1,18 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable',
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
         lazypath,
     })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- need to set up this before lazy load plugins
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-return require('lazy').setup('plugins')
+return require("lazy").setup("plugins")
