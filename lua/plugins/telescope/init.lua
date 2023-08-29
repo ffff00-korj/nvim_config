602d9c9fbd899c73b9d9e5c85fd635cadd263467
iborovkov <iborovkov97@yandex.ru>
2023-08-29 12:55:17 +0300

    add neotree plugin, telescope roll back

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -22,8 +22,8 @@ local config = {
         local builtin = require("telescope.builtin")
         local utils = require("telescope.utils")
 
-        vim.keymap.set("n", "<leader>pf", function() builtin.find_files({ cwd = utils.buffer_dir() }) end, {})
-        vim.keymap.set("n", "<leader>pg", function() builtin.git_files({ cwd = utils.buffer_dir() }) end, {})
+        vim.keymap.set("n", "<leader>pf", function() builtin.find_files() end, {})
+        vim.keymap.set("n", "<leader>pg", function() builtin.git_files() end, {})
         vim.keymap.set("n", "<leader>ps", function()
             builtin.grep_string({
                 search = vim.fn.input("Grep > ")
