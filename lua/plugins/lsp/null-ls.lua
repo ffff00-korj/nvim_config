f9aff775a3d4371fbedf2d7740401a10a4747dec
iborovkov <ilia97borovkov@gmail.com>
2023-09-12 23:54:02 +0300

    fix null-ls, now load corrertly

--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -7,20 +7,20 @@ local options = {
                 return venv_path .. "/bin"
             end
         end
-        if pythonenv then
-            local null_ls = require("null-ls")
-            local formatting = null_ls.builtins.formatting
-            local diagnostics = null_ls.builtins.diagnostics
-            
-            null_ls.setup({
-                sources = {
-                    formatting.isort.with({ prefer_local = python_env }),
-                    formatting.black.with({ prefer_local = python_env }),
-                    diagnostics.flake8.with({ prefer_local = python_env }),
-                    diagnostics.mypy.with({ prefer_local = python_env }),
-                },
-            })
+        if not python_env() then
+            return nil
         end
+        local null_ls = require("null-ls")
+        local formatting = null_ls.builtins.formatting
+        local diagnostics = null_ls.builtins.diagnostics
+        null_ls.setup({
+            sources = {
+                formatting.isort.with({ prefer_local = python_env }),
+                formatting.black.with({ prefer_local = python_env }),
+                diagnostics.flake8.with({ prefer_local = python_env }),
+                diagnostics.mypy.with({ prefer_local = python_env }),
+            },
+        })
     end,
 }
 
