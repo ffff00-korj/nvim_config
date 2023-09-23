519a17556c73a72e3995751a19da6df992a66e98
iborovkov <ilia97borovkov@gmail.com>
2023-09-23 20:14:12 +0300

    try github dark

--- lua/plugins/colors/github-theme.lua
+++ lua/plugins/colors/github-theme.lua
@@ -4,10 +4,11 @@ local options = {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
         require('github-theme').setup({
-            -- ...
+            options = { transparent = true,
+            }
         })
 
-        vim.cmd('colorscheme github_dark_dimmed')
+        vim.cmd('colorscheme github_dark')
     end,
 }
 
