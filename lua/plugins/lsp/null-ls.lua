ade1436aba30c33ed174670ce5aa337fea71452f
iborovkov <iborovkov97@yandex.ru>
2023-09-26 10:02:48 +0300

    refactoring

--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -1,15 +1,6 @@
 local options = {
     "jose-elias-alvarez/null-ls.nvim",
     config = function()
-        local python_env = function()
-            local venv_path = os.getenv("VIRTUAL_ENV")
-            if venv_path then
-                return venv_path .. "/bin"
-            end
-        end
-        if not python_env() then
-            return nil
-        end
         local null_ls = require("null-ls")
         local formatting = null_ls.builtins.formatting
         local diagnostics = null_ls.builtins.diagnostics
