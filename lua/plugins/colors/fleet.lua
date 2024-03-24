f3934d6de9bc10a86b832aaed6eec0f2b8ede242
ilya <iborovkov97@yandex.ru>
2024-03-24 14:24:34 +0300

    disable colorcolumn

--- lua/plugins/colors/fleet.lua
+++ lua/plugins/colors/fleet.lua
@@ -1,6 +1,8 @@
 local options = {
     "felipeagc/fleet-theme-nvim",
-    config = function() vim.cmd("colorscheme fleet") end
+    config = function()
+        vim.cmd("colorscheme fleet")
+    end
 }
 
 return options
