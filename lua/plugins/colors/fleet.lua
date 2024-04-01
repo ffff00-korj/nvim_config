5f7904b21657715f9041ae97839a26280464b51d
ilya <iborovkov97@yandex.ru>
2024-04-01 15:07:43 +0300

    number line color fix

--- lua/plugins/colors/fleet.lua
+++ lua/plugins/colors/fleet.lua
@@ -3,6 +3,7 @@ local options = {
     config = function()
         vim.cmd("colorscheme fleet")
         vim.cmd("hi Normal ctermbg=none guibg=none")
+        vim.cmd("hi clear SignColumn")
     end
 }
 
