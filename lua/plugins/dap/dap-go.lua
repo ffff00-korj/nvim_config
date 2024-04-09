bbddcb359b79961f8d17204460665d80bed423b0
ilborovkov <ilborovkov@ozon.ru>
2024-04-09 12:41:27 +0300

    add nvim dap for go

---
+++ lua/plugins/dap/dap-go.lua
@@ -0,0 +1,16 @@
+local options = {
+    "leoluz/nvim-dap-go",
+    dependencies = {
+        "mfussenegger/nvim-dap",
+    },
+    ft = "go",
+    config = function()
+        require("dap-go").setup()
+
+        vim.keymap.set("n", "<leader><leader>gdm", function()
+            require("dap-go").debug_test()
+        end)
+    end,
+}
+
+return options
---
+++ lua/plugins/dap/init.lua
@@ -0,0 +1,7 @@
+local options = {
+    require("plugins.dap.dap"),
+    require("plugins.dap.dap-ui"),
+    require("plugins.dap.dap-go"),
+}
+
+return options
