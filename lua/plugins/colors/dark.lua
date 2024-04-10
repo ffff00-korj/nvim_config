c391d520609997d23ef54731ee7483232eeff5b5
ilborovkov <ilborovkov@ozon.ru>
2024-04-10 16:00:11 +0300

    git sings prettier colors

--- lua/plugins/colors/dark.lua
+++ lua/plugins/colors/dark.lua
@@ -9,9 +9,9 @@ local options = {
         vim.cmd("hi Pmenu guibg=none guifg=none")
         -- custom git signs
         vim.cmd("hi clear SignColumn")
-        vim.cmd("hi DiffAdd guibg=none guifg=green")
-        vim.cmd("hi DiffChange guibg=none guifg=blue")
-        vim.cmd("hi DiffDelete guibg=none guifg=red")
+        vim.cmd("hi DiffAdd guibg=none guifg=#26A269")
+        vim.cmd("hi DiffChange guibg=none guifg=#61AFEF")
+        vim.cmd("hi DiffDelete guibg=none guifg=#E06C75")
         vim.cmd("hi DiffText guibg=none guifg=gray")
     end
 }
