3bcd6e962d0f604954ab132801bf2f9c4553d400
ilia <iborovkov97@yandex.ru>
2024-04-29 01:52:00 +0300

    dapui configure

--- lua/plugins/dap/dap-ui.lua
+++ lua/plugins/dap/dap-ui.lua
@@ -3,26 +3,67 @@ local options = {
     dependencies = {
         { "mfussenegger/nvim-dap" },
         { "nvim-neotest/nvim-nio" },
+        { "folke/neodev.nvim" },
     },
     keys = {
         "<leader>Du",
-        "<leader>DU",
+        "<leader>Dk",
     },
     config = function()
         local dapui = require("dapui")
+        dapui.setup {
+            layouts = {
+                {
+                    elements = {
+                        "watches",
+                        "scopes",
+                    },
+                    size = 0.25,
+                    position = "right",
+                },
+                {
+                    elements = {
+                        "repl",
+                    },
+                    size = 0.25,
+                    position = "bottom",
+                },
+            },
+        }
 
-        dapui.setup()
-
-        vim.keymap.set("n", "<leader>Du", function()
-            dapui.toggle { layout = 2 }
-        end, {
-            desc = "Toggle [D]ebug [u]I (only debug console and terminal)",
+        require("neodev").setup({
+            library = { plugins = { "nvim-dap-ui" }, types = true },
         })
 
-        vim.keymap.set("n", "<leader>DU", function()
+        vim.keymap.set("n", "<leader>Du", function()
             dapui.toggle()
         end, {
-            desc = "Toggle [D]ebug [U]I (full standart UI)",
+            desc = "Toggle [D]ebug [u]I",
+        })
+        vim.keymap.set({ "n", "v" }, "<leader>Dk", function()
+            dapui.eval()
+        end, {
+            desc = "Evaluate debug expression",
+        })
+        vim.keymap.set("n", "<leader>Dts", function()
+            dapui.float_element("stacks", { enter = true })
+        end, {
+            desc = "[D]ebug [t]oggle [s]tacks",
+        })
+        vim.keymap.set("n", "<leader>Dtb", function()
+            dapui.float_element("breakpoints", { enter = true })
+        end, {
+            desc = "[D]ebug [t]oggle [b]reakpoints",
+        })
+        vim.keymap.set("n", "<leader>Dtw", function()
+            dapui.float_element("watches", { enter = true })
+        end, {
+            desc = "[D]ebug [t]oggle [w]atches",
+        })
+        vim.keymap.set("n", "<leader>Dtl", function()
+            dapui.float_element(nil, { enter = true })
+        end, {
+            desc = "[D]ebug [t]oggle [l]ist",
         })
     end,
 }
