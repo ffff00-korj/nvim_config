6675301494f80a93f6d263fb6c231c8a320d1f7c
ilia <iborovkov97@yandex.ru>
2024-04-30 16:54:39 +0300

    change dap highlights

--- lua/plugins/colors/dark.lua
+++ lua/plugins/colors/dark.lua
@@ -17,6 +17,34 @@ local options = {
         vim.cmd("hi DiffText guibg=none guifg=gray")
         -- change line number font folor
         vim.cmd("hi LineNr guifg=#797979")
+        -- dapui colors. get colors from https://github.com/doums/dark.nvim/blob/main/lua/dark/palette.lua
+        vim.cmd("hi DapUIScope guifg=#20999D") -- type
+        vim.cmd("hi DapUIType guifg=#CF8E6D") -- keyword
+        vim.cmd("hi DapUIValue guifg=gray")
+        vim.cmd("hi DapUIVariable guifg=gray")
+        vim.cmd("hi DapUIModifiedValue guifg=gray")
+        vim.cmd("hi DapUIDecoration guifg=gray")
+        vim.cmd("hi DapUIThread guifg=gray")
+        vim.cmd("hi DapUIStoppedThread guifg=gray")
+        vim.cmd("hi DapUISource guifg=gray")
+        vim.cmd("hi DapUILineNumber  guifg=#797979") -- from LineNr highlight
+        vim.cmd("hi DapUIFloatBorder guifg=none") -- standart border from cmp
+        vim.cmd("hi DapUIWatchesEmpty guifg=gray")
+        vim.cmd("hi DapUIWatchesValue guifg=gray")
+        vim.cmd("hi DapUIWatchesError guifg=#F75464") -- error
+        vim.cmd("hi DapUIBreakpointsPath guifg=gray")
+        vim.cmd("hi DapUIBreakpointsInfo guifg=gray")
+        vim.cmd("hi DapUIBreakpointsCurrentLine guifg=gray")
+        vim.cmd("hi DapUIBreakpointsDisabledLine  guifg=gray")
+        vim.cmd("hi DapUIStepOver guifg=gray")
+        vim.cmd("hi DapUIStepInto guifg=gray")
+        vim.cmd("hi DapUIStepBack guifg=gray")
+        vim.cmd("hi DapUIStepOut guifg=gray")
+        vim.cmd("hi DapUIStop guifg=gray")
+        vim.cmd("hi DapUIPlayPause guifg=gray")
+        vim.cmd("hi DapUIRestart guifg=gray")
+        vim.cmd("hi DapUIUnavailable guifg=gray")
+        vim.cmd("hi DapUIWinSelect guifg=gray")
     end
 }
 
