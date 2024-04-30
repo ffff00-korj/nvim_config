4508720fec1d478290aa128176c43f7119635cd8
ilia <iborovkov97@yandex.ru>
2024-04-30 16:04:33 +0300

    dapui auto on dap launch

--- lua/plugins/dap/dap.lua
+++ lua/plugins/dap/dap.lua
@@ -6,7 +6,20 @@ local options =
     },
     lazy = true,
     config = function()
-        local dap = require "dap"
+        local dap = require("dap")
+        local dapui = require("dapui")
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
         require("nvim-dap-virtual-text").setup()
 
         vim.keymap.set("n", "<F5>", function()
