4230e08ec00a11f289870d59b04b0bd2b2e0ed92
Borovkov Ilya <iborovkov97@yandex.ru>
2024-08-25 23:47:31 +0300

    change minimap settings

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -3,12 +3,20 @@ local options = {
     version = "v3.*.*",
     enabled = true,
     lazy = false,
+    keys = {
+        { "<leader>nm", "<cmd>Neominimap toggle<cr>", desc = "Toggle global [n]eovim [m]inimap" },
+        { "<leader>nr", "<cmd>Neominimap refresh<cr>", desc = "Refresh global [n]eovim [m]inimap" },
+    },
     init = function()
         vim.opt.wrap = false
         vim.opt.sidescrolloff = 36
 
         vim.g.neominimap = {
             auto_enable = true,
+            exclude_filetypes = {
+                "help",
+                "netrw",
+            },
         }
     end,
 }
