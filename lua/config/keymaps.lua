41eebcbbe4098f4db8cd11ccee0441aae0baff95
Borovkov Ilya <iborovkov97@yandex.ru>
2024-12-14 15:16:26 +0300

    delete window navigation remaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,9 +9,3 @@ vim.keymap.set("n", "n", "nzzzv", { desc = "Next search pattern" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Prev search pattern" })
 vim.keymap.set("n", "<A-c>", "<cmd>:bd<CR>", { desc = "Close cur buffer" })
 vim.keymap.set("v", "/", "\"fy/\\V<C-R>f<CR>", { desc = "Find selected words" })
--- window moves
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
---
