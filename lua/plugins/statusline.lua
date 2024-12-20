ccce80c5f43ccd477de46ea78305f716f352e86e
Borovkov Ilya <iborovkov97@yandex.ru>
2024-12-20 19:21:28 +0300

    ref + add status line plugin

---
+++ lua/plugins/statusline.lua
@@ -0,0 +1,10 @@
+local opts = {
+    "echasnovski/mini.nvim",
+    enabled = true,
+    config = function()
+        local statusline = require("mini.statusline")
+        statusline.setup { use_icons = true }
+    end
+}
+
+return opts
