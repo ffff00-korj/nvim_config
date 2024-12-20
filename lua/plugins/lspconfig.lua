6199dd6c2fc580e01b513dd27143bed4baf98edc
Borovkov Ilya <iborovkov97@yandex.ru>
2024-12-20 18:12:23 +0300

    different completion plugin

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,6 +3,7 @@ local opts = {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
+        "saghen/blink.cmp",
         {
             "folke/lazydev.nvim",
             ft = "lua", -- file type
@@ -15,11 +16,14 @@ local opts = {
     },
     config = function()
         local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup {}
-        lspconfig.pyright.setup {}
-        lspconfig.ruff.setup {}
+        local capabilities = require('blink.cmp').get_lsp_capabilities()
+        lspconfig.lua_ls.setup {capabilities = capabilities}
+        lspconfig.pyright.setup {capabilities = capabilities}
+        lspconfig.ruff.setup {capabilities = capabilities}
 
+        ---@diagnostic disable: missing-fields
         vim.keymap.set("n", "<F3>", function() vim.lsp.buf.format() end, { desc = "Format current buffer" })
+        ---@diagnostic enable: missing-fields
     end,
 }
 
